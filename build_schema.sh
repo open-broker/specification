41d9cbc7d3da2f2edeffb9aa0d2809b985f77d6a
William Rudenmalm <me@whn.se>
2018-06-08 23:41:51 +0200

    Add disbursed event

--- build_schema.sh
+++ build_schema.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 ~/go/bin/yaml2json < schema/se/PrivateUnsecuredLoanApplicationCreated.yaml > schema/se/PrivateUnsecuredLoanApplicationCreated.json
+~/go/bin/yaml2json < schema/se/PrivateUnsecuredLoanDisbursed.yaml > schema/se/PrivateUnsecuredLoanDisbursed.yaml
