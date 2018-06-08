b11b986f3beead20697f7eadc43a2cc53ed2d5e9
William Rudenmalm <me@whn.se>
2018-06-08 23:52:59 +0200

    add application status updated

--- build_schema.sh
+++ build_schema.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 ~/go/bin/yaml2json < schema/se/PrivateUnsecuredLoanApplicationCreated.yaml > schema/se/PrivateUnsecuredLoanApplicationCreated.json
-~/go/bin/yaml2json < schema/se/PrivateUnsecuredLoanDisbursed.yaml > schema/se/PrivateUnsecuredLoanDisbursed.yaml
+~/go/bin/yaml2json < schema/se/PrivateUnsecuredLoanDisbursed.yaml > schema/se/PrivateUnsecuredLoanDisbursed.json
+~/go/bin/yaml2json < schema/se/PrivateUnsecuredLoanStatusUpdate.yaml > schema/se/PrivateUnsecuredLoanStatusUpdated.json
