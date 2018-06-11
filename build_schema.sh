06732928103cfa83c86372335668ed8c2d9ba8a9
William Rudenmalm <me@whn.se>
2018-06-11 23:10:32 +0200

    Add offer event

--- build_schema.sh
+++ build_schema.sh
@@ -5,3 +5,4 @@
 ~/go/bin/yaml2json < schema/se/PrivateUnsecuredLoanStatusUpdated.yaml > schema/se/PrivateUnsecuredLoanStatusUpdated.json
 ~/go/bin/yaml2json < schema/se/PrivateUnsecuredLoanRejection.yaml > schema/se/PrivateUnsecuredLoanRejection.json
 ~/go/bin/yaml2json < schema/se/PrivateUnsecuredLoanDelayedProcessing.yaml > schema/se/PrivateUnsecuredLoanDelayedProcessing.json
+~/go/bin/yaml2json < schema/se/PrivateUnsecuredLoanOffering.yaml > schema/se/PrivateUnsecuredLoanOffering.json
