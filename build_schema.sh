d36ccf4a6e47945a99815ba85e50f732d74254bf
William Rudenmalm <me@whn.se>
2018-06-11 12:00:22 +0200

    add Rejection and delayedprocessing

--- build_schema.sh
+++ build_schema.sh
@@ -3,3 +3,5 @@
 ~/go/bin/yaml2json < schema/se/PrivateUnsecuredLoanApplicationCreated.yaml > schema/se/PrivateUnsecuredLoanApplicationCreated.json
 ~/go/bin/yaml2json < schema/se/PrivateUnsecuredLoanDisbursed.yaml > schema/se/PrivateUnsecuredLoanDisbursed.json
 ~/go/bin/yaml2json < schema/se/PrivateUnsecuredLoanStatusUpdated.yaml > schema/se/PrivateUnsecuredLoanStatusUpdated.json
+~/go/bin/yaml2json < schema/se/PrivateUnsecuredLoanRejection.yaml > schema/se/PrivateUnsecuredLoanRejection.json
+~/go/bin/yaml2json < schema/se/PrivateUnsecuredLoanDelayedProcessing.yaml > schema/se/PrivateUnsecuredLoanDelayedProcessing.json
