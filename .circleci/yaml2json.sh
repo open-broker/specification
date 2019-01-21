af751db5794412d41739600466cb5f99b2ea9e34
Anton Österberg <anton.osterberg@gmail.com>
2019-01-21 16:07:21 +0100

    Minor updates to yaml2json.sh

--- .circleci/yaml2json.sh
+++ .circleci/yaml2json.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 FILES=$(/bin/ls -1 $1|grep -E '.ya?ml')
 cd $1 || exit
+echo -e "Converting schema in $1:"
 for f in $FILES
 do
 	NEW_NAME=$(echo $f|cut -d '.' -f 1)
 	NEW_NAME="$NEW_NAME.json"
-	echo "$f -> $NEW_NAME" 
+	echo -e "\t$f -> $NEW_NAME" 
 	yaml2json < $f > $NEW_NAME
 done
