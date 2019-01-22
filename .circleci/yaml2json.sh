d8a59712eaf162a2515a1d5db0222359bbbc7172
Anton Österberg <anton@42x.io>
2019-01-22 17:58:29 +0100

    Add CircleCI build script (#76)

--- .circleci/yaml2json.sh
+++ .circleci/yaml2json.sh
@@ -2,11 +2,12 @@
 FILES=$(/bin/ls -1 $1|grep -E '.ya?ml')
 cd $1 || exit
 ls $2 || exit
+mkdir -p $2/$1
 echo -e "Converting schema in $1:"
 for f in $FILES
 do
 	NEW_NAME=$(echo $f|cut -d '.' -f 1)
 	NEW_NAME="$NEW_NAME.json"
-	echo -e "\t$f -> "$2/$NEW_NAME" 
-	yaml2json < $f > "$2/$NEW_NAME"
+	echo -e "\t$f -> $2/$1/$NEW_NAME" 
+	yaml2json < $f > "$2/$1/$NEW_NAME"
 done
