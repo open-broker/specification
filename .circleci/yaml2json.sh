e0d69cf1aeb3ac744a4f8e585258bc23c935bf7e
Anton Österberg <anton.osterberg@gmail.com>
2019-01-22 12:10:52 +0100

    CircleCI use workflows and executors

--- .circleci/yaml2json.sh
+++ .circleci/yaml2json.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 FILES=$(/bin/ls -1 $1|grep -E '.ya?ml')
 cd $1 || exit
+ls $2 || exit
 echo -e "Converting schema in $1:"
 for f in $FILES
 do
 	NEW_NAME=$(echo $f|cut -d '.' -f 1)
 	NEW_NAME="$NEW_NAME.json"
-	echo -e "\t$f -> $NEW_NAME" 
-	yaml2json < $f > $NEW_NAME
+	echo -e "\t$f -> "$2/$NEW_NAME" 
+	yaml2json < $f > "$2/$NEW_NAME"
 done
