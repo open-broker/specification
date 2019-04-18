3f33c91b0403739430202841877296610c4d2187
Anton Österberg <anton.osterberg@gmail.com>
2019-04-18 12:33:27 +0200

    Add comment in script and display error message if ajv is not installed.

--- .circleci/test_examples.sh
+++ .circleci/test_examples.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env sh
+# This script requires NPM library ajv
+# in order to test the examples against a json schema.
+# Install it with 'npm -g install ajv'
+
+if ! which ajv; then
+    echo -n "ajv is not installed, install it with "
+    echo "'npm -g install ajv' to run this script"
+    exit 3
+fi
+
 DIR="/tmp/workspace/json"
 cd $1 || exit
 EXAMPLES="example"
